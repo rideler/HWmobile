30eee81c372c64a94ab647035b4fcc85c7b72a28
Shay Kremer <kremersh@imgosx.local>
2016-11-13 10:57:18 +0200

    Initial Commit

---
+++ project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Shay Kremer on 13/11/2016.
+//  Copyright © 2016 Shay Kremer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
